5b9da8694eb4dbcad0e784f9047a734033db1745
Andy <andiiidim0@gmail.com>
2025-02-09 19:42:01 +0200

    Commit

--- src/main/java/com/example/AestiqueClothing/Policies/PolicyController.java
+++ src/main/java/com/example/AestiqueClothing/Policies/PolicyController.java
@@ -6,13 +6,23 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/policies")
 public class PolicyController {
-    @GetMapping("/privacy")
-    public String privacyPolicy() {
+    @GetMapping("/privacy-policy")
+    public String getPrivacyPolicy() {
         return "policy/privacy-policy";
     }
 
-    @GetMapping("/terms")
-    public String termsOfUse() {
-        return "terms-of-use";
+    @GetMapping("/terms-conditions")
+    public String getTermsAndConditions() {
+        return "policy/terms-conditions";
+    }
+
+    @GetMapping("/return-policy")
+    public String getReturnPolicy() {
+        return "policy/return-policy";
+    }
+
+    @GetMapping("/faqs")
+    public String getFAQsPolicy() {
+        return "policy/faqs";
     }
 }
